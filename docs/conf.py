b195d5202f4822d1f9db4afb1555f5fc1d29b29a
poulp <amathieu@protonmail.com>
2017-07-08 09:57:36 +0200

    Mock import of Gtk in docs to avoid error in read the docs

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,19 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
+from unittest.mock import MagicMock
+import gi
 
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return MagicMock()
+
+
+gi.require_version = MagicMock(return_value=True)
+MOCK_MODULES = ['Gtk']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
